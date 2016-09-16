22d67cf61ee6604c35d5150b7d60f0b8e5065a3b
HK Kahng <hk@router-rooter.com>
2016-09-16 09:05:49 -0700

    add code to meet first specification

---
+++ src/RepeatCounter.php
@@ -0,0 +1,13 @@
+<?php
+    class RepeatCounter
+    {
+
+        function wordMatch($input1, $input2)
+        {
+            if ($input1 == $input2) {
+                return true;
+            }
+        }
+
+    }
+?>
--- tests/Test.php
+++ tests/Test.php
@@ -1,34 +1,33 @@
 
 <?php
-    require_once "src/Src.php";
+    require_once "src/RepeatCounter.php";
 
-    class SrcTest extends PHPUnit_Framework_TestCase
-    //run test in terminal: ./vendor/bin/phpunit tests
-    //on Mac: run: export PATH=$PATH:./vendor/bin
-    //then run phpunit tests
+    class RepeatCounterTest extends PHPUnit_Framework_TestCase
 
     {
 
-      function test_score_letter_1 ()
+      function test_word_match()
       {
-        $test_onepoint_letters = new Game;
-        $input_array = array('a','e','i','o','u','l','n','r','s','t');
-        $expected_result = [1,1,1,1,1,1,1,1,1,1];
-        $result_array = array();
-        foreach ($input_array as $letter) {
-            $result = $test_onepoint_letters->scoreLetter($letter);
-            array_push($result_array, $result);
-        }
-        $this->assertEquals($result_array, $expected_result);
+        $test_wordscore = new RepeatCounter;
+        $input1 = 'hello';
+        $input2 = 'hello';
+        $result = $test_wordscore->wordMatch($input1, $input2);
+        $this->assertEquals(true, $result);
       }
 
-      function test_word_score()
-      {
-        $test_wordscore = new Game;
-        $input = 'dog';
-        $result = $test_wordscore->scoreWord($input);
-        $this->assertEquals(5, $result);
-      }
+      // function test_score_letter_1 ()
+      // {
+      //   $test_onepoint_letters = new Game;
+      //   $input_array = array('a','e','i','o','u','l','n','r','s','t');
+      //   $expected_result = [1,1,1,1,1,1,1,1,1,1];
+      //   $result_array = array();
+      //   foreach ($input_array as $letter) {
+      //       $result = $test_onepoint_letters->scoreLetter($letter);
+      //       array_push($result_array, $result);
+      //   }
+      //   $this->assertEquals($result_array, $expected_result);
+      // }
+
 
    }
  ?>
